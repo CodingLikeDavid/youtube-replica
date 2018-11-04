b6b18ea1922fab8f70e5b1134305006907672e6e
Dávid Csejtei <csejteidavid90@gmail.com>
2018-11-04 15:40:25 +0100

    Top toolbar WIP

--- src/app.js
+++ src/app.js
@@ -1,20 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars, faSearch} from "@fortawesome/free-solid-svg-icons";
+
+import logo from '../img/logo.png';
 import sampleImage from '../img/sampleimg.jpg';
 
 export default class App extends Component {
 
     getTopToolbar() {
-        return(
+        return (
             <div className="row">
                 <div className="col">
                     <div className="top-toolbar container-fluid">
                         <div className="row">
-                            <div className="col bg-secondary">
-                                <div className="menu">menu</div>
-                                <div className="icon">icon</div>
+                            <div className="col">
+                                <div className="menu">
+                                    <FontAwesomeIcon icon={faBars}
+                                                     className='fa-3x icon'/>
+                                    <a href="#">
+                                        <img src={logo} width={80}/>
+                                    </a>
+                                </div>
                             </div>
-                            <div className="col-6 bg-light">02</div>
+                            <div className="col-4 text-center bg-light">
+                                {this.getSearchBar()}
+                            </div>
                             <div className="col bg-info">03</div>
                         </div>
                     </div>
@@ -23,8 +34,22 @@ export default class App extends Component {
         );
     }
 
+    getSearchBar() {
+        return (
+            <div className="row search-area pr-2 pl-2">
+                <div className="col-11">
+                    <input type="text" placeholder="Keresés"/>
+                </div>
+                <div className="col">
+                    <FontAwesomeIcon icon={faSearch}
+                                     className='fa-2x search-btn'/>
+                </div>
+            </div>
+        );
+    }
+
     getContentArea() {
-        return(
+        return (
             <div className="row pt-3">
                 <div className="col">
                     <div className="container">
@@ -32,12 +57,12 @@ export default class App extends Component {
                             <div className="col">
                                 <h1>Ajánlott</h1>
                                 <div className="row boxes">
-                                    { this.getBox() }
-                                    { this.getBox() }
-                                    { this.getBox() }
-                                    { this.getBox() }
-                                    { this.getBox() }
-                                    { this.getBox() }
+                                    {this.getBox()}
+                                    {this.getBox()}
+                                    {this.getBox()}
+                                    {this.getBox()}
+                                    {this.getBox()}
+                                    {this.getBox()}
                                 </div>
                             </div>
                         </div>
@@ -48,7 +73,7 @@ export default class App extends Component {
     }
 
     getBox() {
-        return(
+        return (
             <div className="col card m-1">
                 <img className="card-img-top" src={sampleImage}/>
                 <div className="card-body p-0">
@@ -64,10 +89,10 @@ export default class App extends Component {
     }
 
     render() {
-        return(
+        return (
             <div className='container-fluid'>
-                { this.getTopToolbar() }
-                { this.getContentArea() }
+                {this.getTopToolbar()}
+                {this.getContentArea()}
             </div>
         );
     }
